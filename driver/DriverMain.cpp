8b8d6e33bbc3fa337ff78e02b4399f2545852a0e
Florian Erler <ethon@ethon.cc>
2015-04-18 21:25:55 +0200

    First commit #2

--- driver/DriverMain.cpp
+++ driver/DriverMain.cpp
@@ -1,3 +1,23 @@
+// Copyright (c) <2015> <Florian Erler>
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising
+// from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented;
+// you must not claim that you wrote the original software.
+// If you use this software in a product, an acknowledgment in the product
+// documentation would be appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such, and must not
+// be misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source distribution.
+
 // C++ Standard Library:
 #include <iostream>
 #include <cstdlib>
@@ -27,38 +47,36 @@ void ed::driverif::init() {
       << applicationPath << "'" << std::endl;
 
    // Try to load the creator function from the shared library.
+   SharedLibraryPtr library;
    ed::ApplicationCreatorFunction creator = nullptr;
    try {
-      applicationLibrary = ed::loadSharedLibrary(applicationPath);
+      library = ed::loadSharedLibrary(applicationPath);
       creator = ed::toFunctionPointer<ed::Application*>(
-         applicationLibrary->getSymbol(SYMNAME_CREATOR));
-   } catch(ed::SharedLibraryError const& e) {
+         library->getSymbol(SYMNAME_CREATOR));
+   } catch(std::exception const& e) {
       std::cerr << "[ERROR] Ed von Hack: Failed loading application (" <<
-         e.what() << ": " << e.getSystemError() << ")" << std::endl;
-      if(!creator) {
-         applicationLibrary.reset();
-      }
+         e.what() << ")" << std::endl;
       return;
    }
 
    // Create the application via the creator function.
    try {
       application.reset(creator());
+      if(!application) {
+         std::cerr
+            << "[ERROR] Ed von Hack: Application could not be created"
+            << std::endl;
+         return;
+      }
       application->init();
    } catch(std::exception const& e) {
       std::cerr << "[ERROR] Ed von Hack: Application could not be created (" <<
          e.what() << ")" << std::endl;
-      applicationLibrary.reset();
-      return;
-   }
-   if(!application) {
-      std::cerr <<
-         "[ERROR] Ed von Hack: Application could not be created" << std::endl;
-      applicationLibrary.reset();
       return;
    }
 
    // Yay, the application was created.
+   applicationLibrary = std::move(library);
    std::cout <<
       "[INFO] Ed von Hack: Successfully loaded application" << std::endl;
 }
